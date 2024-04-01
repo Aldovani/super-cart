9e068fa30bdb70af24039a608da00fe9841381a7
Aldovani <aldovanihcosta@gmail.com>
2024-04-01 00:25:17 -0300

    fix: Eslint and remove content from index page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+import './globals.css'
 
 const poppins = Poppins({
-  weight: ["400", "300", "500", "600", "800"],
-  subsets: ["latin"],
-});
+  weight: ['400', '300', '500', '600', '800'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Super cart",
-  description: "Uma nova maneira de realizar suas compras",
-};
+  title: 'Super cart',
+  description: 'Uma nova maneira de realizar suas compras',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br">
       <body className={poppins.className}>{children}</body>
     </html>
-  );
+  )
 }
