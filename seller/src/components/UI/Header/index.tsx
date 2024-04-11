7b78f3d1cdbf542266af4638c163ae65123fa221
Aldovani <aldovanihcosta@gmail.com>
2024-04-11 00:45:49 -0300

    feat: add Header component

---
+++ seller/src/components/UI/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="pl-10 text-gray-500 row-start-1 row-end-2 col-start-2 col-end-3 flex items-center border-b border-gray-200">
+      <span>Dashboard /</span>
+    </header>
+  )
+}
