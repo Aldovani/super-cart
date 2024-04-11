638468e2154a43bb95ff18a2dbe787d34c878c6a
Aldovani <aldovanihcosta@gmail.com>
2024-04-11 01:08:58 -0300

    feat: add Products Page

---
+++ seller/src/app/(main)/products/page.tsx
@@ -0,0 +1,26 @@
+import { Link } from '@/components/UI/Link'
+import { ProductsFilter } from '@/components/UI/ProductsFilter'
+
+export default function ProductListPage() {
+  return (
+    <div className="">
+      <header className="px-8  mt-10 ">
+        <div className="border-b pb-6 border-gray-200 flex justify-between  items-center   ">
+          <div>
+            <h2 className="text-gray-800 font-medium text-[2rem]">Produtos</h2>
+            <p className="text-gray-400 ">
+              Confira todos os produtos registrado no seu marktplace
+            </p>
+          </div>
+          <Link className="w-fit px-8 " href="/products/create" passHref>
+            Adicionar produto
+          </Link>
+        </div>
+      </header>
+
+      <section>
+        <ProductsFilter />
+      </section>
+    </div>
+  )
+}
