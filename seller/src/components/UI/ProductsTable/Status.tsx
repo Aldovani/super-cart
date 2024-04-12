af44abea043d181dfc2bd3d3585a72f6748fa574
Aldovani <aldovanihcosta@gmail.com>
2024-04-12 16:34:22 -0300

    feat: create Status product table

---
+++ seller/src/components/UI/ProductsTable/Status.tsx
@@ -0,0 +1,27 @@
+import { tv, VariantProps } from 'tailwind-variants'
+
+const status = tv({
+  base: 'font-medium px-3 py-1',
+  variants: {
+    status: {
+      unavailable: 'text-rose-500 bg-rose-50',
+      available: 'text-emerald-600 bg-emerald-50',
+      preparation: 'text-yellow-500 bg-yellow-50',
+    },
+  },
+  defaultVariants: {
+    status: 'unavailable',
+  },
+})
+
+type StatusProps = VariantProps<typeof status>
+
+export function Status({ status: VStatus }: StatusProps) {
+  return (
+    <span className={status({ status: VStatus })}>
+      {VStatus === 'unavailable' && 'Indisponível'}
+      {VStatus === 'available' && 'Disponível'}
+      {VStatus === 'preparation' && 'Em preparo'}
+    </span>
+  )
+}
