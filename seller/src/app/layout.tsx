ead3e7b10370362dfcfeead7530d58bf49dcd076
Aldovani <aldovanihcosta@gmail.com>
2024-04-26 00:57:29 -0300

    update

--- seller/src/app/layout.tsx
+++ seller/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-import { ViewTransitions } from 'next-view-transitions'
-
-import { Providers } from '@/providers'
 
 import { Providers } from '@/providers'
 
